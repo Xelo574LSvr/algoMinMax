8031a9595fe9da55dbb9d9a7d478632234be7cf8
dorian.vidal <12811281d@gmail.com>
2026-01-20 13:21:49 +0100

    Mise en place gitignore

---
+++ Joueur.py
@@ -0,0 +1,11 @@
+grille = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+]
+
+print(str(grille[0][0]) + "  | " + str(grille[0][1]) + " |  " + str(grille[0][2]))
+print("---+---+---")
+print(str(grille[1][0]) + "  | " + str(grille[1][1]) + " |  " + str(grille[1][2]))
+print("---+---+---")
+print(str(grille[2][0]) + "  | " + str(grille[2][1]) + " |  " + str(grille[2][2]))
